85291db0c5742e9b5ba0f28c796dfa32180e4cff
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-17 22:28:13 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 from trenalyze_python import Trenalyze
+
+token = "w0bW6GuaQjKUog5GXOJb"
+sender = "2347019491161"
+receiver = "2348157002782"
+msgtext = "From Trenalyze Python Module"
+
+trenalyze = Trenalyze(token, sender, receiver, msgtext, True)
+
+print(trenalyze.send())
