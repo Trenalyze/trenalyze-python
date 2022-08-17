5552808b3d9b3c14ab4d99ad2589afade9f38507
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-17 22:48:29 +0100

    Update __init__.py

--- trenalyze_python/__init__.py
+++ trenalyze_python/__init__.py
@@ -49,23 +49,54 @@ class Trenalyze:
         self.receiver = receiver
     
     def setMsgtext(self, msgtext):
+        """
+        It sets the message text.
+        
+        :param msgtext: The text of the message
+        """
+
+        # Setting the message text.
         self.msgtext = msgtext
 
     def setDebug(self, debug = False):
+        """
+        It sets the debug flag to True or False.
+        
+        :param debug: If True, the debug mode is enabled, defaults to False (optional)
+        """
+
+        # Setting the debug flag to True or False.
         self.debug = debug
 
     def __getUrl(self):
+        """
+        It returns the url of the Trenalyze API.
+        """
+
+        # Setting the url of the Trenalyze API.
         url = "https://trenalyze.com/api/send"
+
+        # Returning the url of the Trenalyze API.
         return url
 
     def __sendRequest(self):
+        """
+        It sends a request.
+        """
 
+        # Sending a request to the Trenalyze API.
         try:
+
+           # Creating a case insensitive dictionary.
             headers = CaseInsensitiveDict()
+
+           # Setting the content type of the request to json.
             headers['Content-Type'] = 'application/json'
 
+           # Setting the url of the Trenalyze API.
             url = self.__getUrl()
 
+            # Creating a dictionary with the keys sender, token, receiver and msgtext.
             data = {
                 "sender": self.sender,
                 "token": self.token,
@@ -73,21 +104,56 @@ class Trenalyze:
                 "msgtext": self.msgtext
             }
             
+            # Sending a post request to the Trenalyze API.
             resp = requests.post(url = self.__getUrl(), data=json.dumps(data), headers=headers)
+
+           # Checking if the request was successful.
             if resp.status_code == 200:
+
+                # Returning the response of the request in json format.
                 return resp.json()
+
+            # Checking if the request was successful. If it was successful, it returns the response of
+            # the request in json format. If it was not successful, it returns an error message.
             else:
+
+                # Checking if the debug flag is set to True.
                 if self.debug == True:
+
+                    # Returning an error message.
                     return "Error: {}".format(resp.status_code)
+
+               # Returning an error message if the request was not successful.
                 else:
+
+                    # Returning an error message if the request was not successful.
                     return "Sorry an Error Occured. Kindly enable debug mode to see more details"
+
+       # Catching any exception that might occur and returning an error message.
         except Exception as e:
+
+           # It checks if the debug flag is set to True.
             if self.debug == True:
+
+                # Returning an error message.
                 return "Error: {}".format(e)
+
+           # It returns an error message if the request was not successful.
             else:
+
+                # Returning an error message if the request was not successful.
                 return "Sorry an Error Occured. Kindly enable debug mode to see more details"
 
     def sendMessage(self):
+        """
+        If the sender is empty, return "Sender cannot be empty". If the receiver is empty, return
+        "Receiver cannot be empty". If the sender is not a number, return "Sender must me a valid Phone
+        Number". If the receiver is not a number, return "Receiver must me a valid Phone Number". If the
+        token is empty, return "Token is Required". If the token length is less than 20, return "Invalid
+        Token. Kindly Re Confirm Token". Otherwise, return the result of the __sendRequest() function
+        :return: The return value is the result of the __sendRequest() method.
+        """
+
         #make sure sender and receiver are numbers and its not empty
         if self.sender == "":
             return "Sender cannot be empty"
@@ -108,9 +174,10 @@ class Trenalyze:
         if self.token == "":
             return "Token is Required"
 
-        #if token length is less than 32
+        #if token length is less than 20
         if len(self.token) < 20:
             return "Invalid Token. Kindly Re Confirm Token"
             
+        # Returning the result of the __sendRequest() method.
         return self.__sendRequest()
 
