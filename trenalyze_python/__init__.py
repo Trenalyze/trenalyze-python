53ae24cf24306439d61f6f5bd39938a1c4c293be
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-17 22:28:09 +0100

    Update __init__.py

--- trenalyze_python/__init__.py
+++ trenalyze_python/__init__.py
@@ -1,31 +1,72 @@
 import requests
 from requests.structures import CaseInsensitiveDict
+import json
 
 class Trenalyze:
-    def __init__(self, token, sender, receiver, msgtext):
+    def __init__(self, token, sender, receiver, msgtext, debug=False):
         self.token = token
         self.sender = sender
         self.receiver = receiver
         self.msgtext = msgtext
+        self.debug = debug
 
     def __getUrl(self):
         url = "https://trenalyze.com/api/send"
         return url
 
     def __sendRequest(self):
-        data = """
-        {
-            "token": self.token,
-            "sender": self.sender,
-            "receiver": self.receiver,
-            "msgtext": self.msgtext
-        }
-        """
-        headers = CaseInsensitiveDict()
-        headers['Content-Type'] = 'application/json'
-        resp = requests.post(self.__getUrl(), headers=headers, data=data)
-        return resp.status_code
-    
+
+        try:
+            headers = CaseInsensitiveDict()
+            headers['Content-Type'] = 'application/json'
+
+            url = self.__getUrl()
+
+            data = {
+                "sender": self.sender,
+                "token": self.token,
+                "receiver": self.receiver,
+                "msgtext": self.msgtext
+            }
+            
+            resp = requests.post(url = self.__getUrl(), data=json.dumps(data), headers=headers)
+            if resp.status_code == 200:
+                return resp.json()
+            else:
+                if self.debug == True:
+                    return "Error: {}".format(resp.status_code)
+                else:
+                    return "Sorry an Error Occured. Kindly enable debug mode to see more details"
+        except Exception as e:
+            if self.debug == True:
+                return "Error: {}".format(e)
+            else:
+                return "Sorry an Error Occured. Kindly enable debug mode to see more details"
+                
     def send(self):
+        #make sure sender and receiver are numbers and its not empty
+        if self.sender == "":
+            return "Sender cannot be empty"
+
+        #if receiver is empty
+        if self.receiver == "":
+            return "Receiver cannot be empty"
+
+        #if sender is not a number
+        if not self.sender.isdigit():
+            return "Sender must me a valid Phone Number"
+
+        #if receiver is not a number
+        if not self.receiver.isdigit():
+            return "Receiver must me a valid Phone Number"
+
+        #if token is empty
+        if self.token == "":
+            return "Token is Required"
+
+        #if token length is less than 32
+        if len(self.token) < 20:
+            return "Invalid Token. Kindly Re Confirm Token"
+            
         return self.__sendRequest()
 
